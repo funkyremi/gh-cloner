1566779c51578c5c5901b2d0862864d65bdfc7e1
funkyremi <remi.testa@gmail.com>
2019-12-23 23:39:37 +0100

    First version

---
+++ README.md
@@ -0,0 +1,9 @@
+# Github Backup
+
+Clone all repos of an organization with one command.
+
+## Usage
+
+`npx gh-cloner`
+
+You will be asked for the Organization name and your Github personal access token ([create one here](https://help.github.com/en/github/authenticating-to-github/creating-a-personal-access-token-for-the-command-line))
---
+++ index.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+require("events").EventEmitter.prototype._maxListeners = 100;
+require("commander").version("1.0.0");
+const Octokit = require("@octokit/rest");
+const shell = require("shelljs");
+const inquirer = require("inquirer");
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "organization",
+      message: "Github Organization Name"
+    },
+    { type: "input", name: "token", message: "Your Github Access Token" }
+  ])
+  .then(answers => {
+    if (!shell.which("git")) {
+      shell.echo("Sorry, this script requires git");
+      shell.exit(1);
+    }
+    const octokit = new Octokit({ auth: answers.token });
+    octokit.repos.listForOrg({ org: answers.organization }).then(({ data }) => {
+      shell.mkdir("-p", answers.organization);
+      shell.cd(answers.organization);
+      data.forEach(r => {
+         shell.exec(`git clone https://${answers.token}@github.com/${r.full_name}.git`, { async: true });
+      });
+    });
+  });
